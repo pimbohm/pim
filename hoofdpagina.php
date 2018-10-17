39b407a34f87aa1ed64603b66d7cb8a0ea04ca10
pimbohm <43641554+pimbohm@users.noreply.github.com>
2018-10-17 09:06:10 +0200

    Add files via upload

---
+++ hoofdpagina.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Title </title>
+    <link rel="stylesheet" href="css/s.css">
+</head>
+
+<body>
+    <header>
+        <a href="index.php">
+            <h1>Incrowd</h1>
+        </a>
+    </header>
+    <nav>
+        <a href="hoofdpaginaa.php">
+        <img src="img/arrow_left.jpg">
+        </a>
+    </nav>
+    <article>
+        <div id="container">
+            <div class="child">Profiel gegevens</div>
+            <div class="child">Naam applicatie</div>
+        </div>
+        <div id="blok">
+            <div class="kind">App icoon</div>
+            <div class="kind">Menu afbeeldingen</div>
+        </div>
+        <div id="tegel">
+            <div class="zoon">Overige afbeeldingen</div>
+            <div class="zoon">Lettertype</div>
+        </div>
+        <div id="kubus">
+            <div class="lid">Primaire kleuren</div>
+            <div class="lid">Secondaire kleuren</div>
+        </div>
+        <div id="vierkant">
+            <div id="klein">App aanvragen</div>
+        </div>
+    </article>
+
+</body>
+
+</html>
